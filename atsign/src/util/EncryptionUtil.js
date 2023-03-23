5d42332cc22517c73372d828b0ba0a3eb6f7118a
lahasya <lahasya.lnu001@umb.edu>
2023-03-23 18:13:20 -0400

    changes in encryption module

--- atsign/src/util/EncryptionUtil.js
+++ atsign/src/util/EncryptionUtil.js
@@ -1,17 +1,21 @@
 
-var EncryptionUtil = {
-    base64Encode: function base64Encode(data) {
-        let bufferObj = Buffer.from(data, "utf8");
 
-        // Encoding into base64
-        let base64String = bufferObj.toString("base64");
-        return base64String;
-    },
-    base64Decode: function base64Decode(data) {
-        let bufferObj = Buffer.from(data, "base64");
-        let string = bufferObj.toString("utf8");
-        return string;
-    }
+module.exports = {
+    base64Encode: base64Encode,
+    base64Decode: base64Decode
+};
+function base64Encode(data) {
+    let bufferObj = Buffer.from(data, "utf8");
+
+    // Encoding into base64
+    let base64String = bufferObj.toString("base64");
+    return base64String;
 }
 
-module.exports = EncryptionUtil;
+function base64Decode(data) {
+    let bufferObj = Buffer.from(data, "base64");
+    let string = bufferObj.toString("utf8");
+    return string;
+}
+
+
