8ebb549120d737b29f2901b0a901bdf507bee595
lahasya <lahasya.lnu001@umb.edu>
2023-03-23 17:11:50 -0400

    Simple BAse64 encode and decode

---
+++ test/EncryptionTest.js
@@ -0,0 +1,26 @@
+var EncryptionUtil = require("../at_client/src/util/EncryptionUtil");
+var expect    = require("chai").expect;
+
+
+describe("Base64EncodeDecode Testing", function() {
+    describe("Testing Base64 Encoding", function(){
+        it("Returns an Encoding String", function(){
+           
+            encodeData= EncryptionUtil.base64Encode("Testing Base64 Encoded Data");
+            
+            expect(encodeData).to.equal("VGVzdGluZyBCYXNlNjQgRW5jb2RlZCBEYXRh");
+        })
+    })
+
+
+    describe("Testing Base64 Decoding", function(){
+        it("Returns a Decoded String", function(){
+           
+            encodeData= EncryptionUtil.base64Encode("Testing Base64 Decoding Function");
+            result = EncryptionUtil.base64Decode(encodeData);
+            expect(result).to.equal("Testing Base64 Decoding Function")
+        })
+    })
+
+
+})
