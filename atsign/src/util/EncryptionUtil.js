94868a3bfc991dfddce2e9626421a94f3b64f867
Samyak Gangwal <sam.gangwal97@gmail.com>
2023-03-26 15:21:17 -0400

    Added AES key generation and key encoding code

--- atsign/src/util/EncryptionUtil.js
+++ atsign/src/util/EncryptionUtil.js
@@ -1,76 +1,101 @@
-
-
 module.exports = {
-    base64Encode: base64Encode,
-    base64Decode: base64Decode,
-    generateRSAKeyPair:generateRSAKeyPair,
-    rsaEncryptToBase64: rsaEncryptToBase64,
-    _signSHA256RSA:_signSHA256RSA,
-    verifying:verifying
+  base64Encode: base64Encode,
+  base64Decode: base64Decode,
+  generateRSAKeyPair: generateRSAKeyPair,
+  rsaEncryptToBase64: rsaEncryptToBase64,
+  _signSHA256RSA: _signSHA256RSA,
+  verifying: verifying,
+  generateAESKeyBase64: generateAESKeyBase64,
+  aesEncryptToBase64: aesEncryptToBase64,
 };
 function base64Encode(data) {
-    let bufferObj = Buffer.from(data, "utf8");
+  let bufferObj = Buffer.from(data, "utf8");
 
-    // Encoding into base64
-    let base64String = bufferObj.toString("base64");
-    return base64String;
+  // Encoding into base64
+  let base64String = bufferObj.toString("base64");
+  return base64String;
 }
 
 function base64Decode(data) {
-    let bufferObj = Buffer.from(data, "base64");
-    let string = bufferObj.toString("utf8");
-    return string;
+  let bufferObj = Buffer.from(data, "base64");
+  let string = bufferObj.toString("utf8");
+  return string;
 }
 
 //RSA KeyPair Generation
 const crypto = require("crypto");
 function generateRSAKeyPair() {
-    return { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
-        modulusLength: 2048,
-        publicKeyEncoding: {
-          type: 'spki',
-          format: 'pem'
-        },
-        privateKeyEncoding: {
-          type: 'pkcs8',
-          format: 'pem'
-        }
-    });
+  return ({ publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+      type: "spki",
+      format: "pem",
+    },
+    privateKeyEncoding: {
+      type: "pkcs8",
+      format: "pem",
+    },
+  }));
 }
 
 //RSA Encryption
-function rsaEncryptToBase64(dataToBeEncrypted,publicKey) {
-    let encryptedText = crypto.publicEncrypt(
-        {
-            key: publicKey,
-            padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-            oaepHash: "sha256",
-        },
-        Buffer.from(dataToBeEncrypted)
-    );
-    let base64Encode_RSA = encryptedText.toString("base64")
-    return base64Encode_RSA
+function rsaEncryptToBase64(dataToBeEncrypted, publicKey) {
+  let encryptedText = crypto.publicEncrypt(
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    Buffer.from(dataToBeEncrypted)
+  );
+  let base64Encode_RSA = encryptedText.toString("base64");
+  return base64Encode_RSA;
 }
 
 //Signing
-function _signSHA256RSA(value,privateKey){
-    const signature = crypto.sign("sha256", Buffer.from(value), {
-        key: privateKey,
-        padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
-    })
-    return signature.toString("base64")
+function _signSHA256RSA(value, privateKey) {
+  const signature = crypto.sign("sha256", Buffer.from(value), {
+    key: privateKey,
+    padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+  });
+  return signature.toString("base64");
 }
 
 //Verifying the signature
-function verifying(value,publicKey,signature){
-    const isVerified = crypto.verify(
-        "sha256",
-        Buffer.from(value),
-        {
-            key: publicKey,
-            padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
-        },
-        signature
-    )
-    return isVerified
- }
+function verifying(value, publicKey, signature) {
+  const isVerified = crypto.verify(
+    "sha256",
+    Buffer.from(value),
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+    },
+    signature
+  );
+  return isVerified;
+}
+
+function generateAESKeyBase64() {
+  return [
+    base64Encode(crypto.randomBytes(32)),
+    base64Encode(crypto.randomBytes(16)),
+  ];
+}
+
+// TODO: aes decryption
+function aesDecryptFromBase64() {
+  cipherTextBase64, keyBase64;
+}
+
+function aesEncryptToBase64(clearText, keyBase64, iv) {
+  const key = Buffer.from(keyBase64, "base64");
+  const initializationVector = Buffer.from(iv, "base64");
+  const cipher = crypto.createCipheriv(
+    "aes-256-ctr",
+    key,
+    initializationVector
+  );
+  let encrypted = cipher.update(clearText, "utf8", "binary");
+  encrypted += cipher.final("binary");
+  return base64Encode(encrypted);
+}
--- test/atsign/util/EncryptionTest.js
+++ test/atsign/util/EncryptionTest.js
@@ -1,26 +1,38 @@
 var EncryptionUtil = require("../../../atsign/src/util/EncryptionUtil");
-var expect    = require("chai").expect;
+var expect = require("chai").expect;
 
+describe("Base64EncodeDecode Testing", function () {
+  describe("Testing Base64 Encoding", function () {
+    it("Returns an Encoding String", function () {
+      encodeData = EncryptionUtil.base64Encode("Testing Base64 Encoded Data");
 
-describe("Base64EncodeDecode Testing", function() {
-    describe("Testing Base64 Encoding", function(){
-        it("Returns an Encoding String", function(){
-           
-            encodeData= EncryptionUtil.base64Encode("Testing Base64 Encoded Data");
-            
-            expect(encodeData).to.equal("VGVzdGluZyBCYXNlNjQgRW5jb2RlZCBEYXRh");
-        })
-    })
+      expect(encodeData).to.equal("VGVzdGluZyBCYXNlNjQgRW5jb2RlZCBEYXRh");
+    });
+  });
 
+  describe("Testing Base64 Decoding", function () {
+    it("Returns a Decoded String", function () {
+      encodeData = EncryptionUtil.base64Encode(
+        "Testing Base64 Decoding Function"
+      );
+      result = EncryptionUtil.base64Decode(encodeData);
+      expect(result).to.equal("Testing Base64 Decoding Function");
+    });
+  });
+});
 
-    describe("Testing Base64 Decoding", function(){
-        it("Returns a Decoded String", function(){
-           
-            encodeData= EncryptionUtil.base64Encode("Testing Base64 Decoding Function");
-            result = EncryptionUtil.base64Decode(encodeData);
-            expect(result).to.equal("Testing Base64 Decoding Function")
-        })
-    })
+describe("AES EncodeDecode testing", function () {
+  describe("AES encode testing", function () {
+    it("Returns an encoded string", function () {
+      encodeData = EncryptionUtil.aesEncryptToBase64(
+        "Testing AES Encoded Data",
+        "7KgXQdb4xGSRG5serDpQt7nh2yxukGpNwa+ZMJsO31Y=",
+        "U+IpZE3OmfSUvSEvf1JLzg=="
+      );
 
-
-})
+      expect(encodeData).to.equal(
+        "UwnDu8KURMOYw4YCw7obw7nDgxU+w6Qrw6MRfjLCkDJ3SQ=="
+      );
+    });
+  });
+});
